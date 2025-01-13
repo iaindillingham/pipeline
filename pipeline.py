2daed33b569f486074f20af63502729c55f4fd85
Iain Dillingham <iain@dillingham.me.uk>
2025-01-13 15:42:19 +0000

    Add `transform` stage to pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,18 @@
+import collections
+import datetime
+
 import requests
+
+
+Book = collections.namedtuple("Book", "publish_date")
 
 
 def extract():
     response = requests.get("https://example.com/")
     page = response.json()
     yield from page["records"]
+
+
+def transform(record):
+    publish_date = datetime.date.fromisoformat(record["publish_date"])
+    return Book(publish_date)
--- test_pipeline.py
+++ test_pipeline.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 
 import pipeline
@@ -12,3 +14,9 @@ def test_extract(monkeypatch):
     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: MockResponse())
     record = next(pipeline.extract())
     assert record["publish_date"] == "2025-01-01"
+
+
+def test_transform():
+    record = {"publish_date": "2025-01-01"}
+    book = pipeline.transform(record)
+    assert book == pipeline.Book(datetime.date(2025, 1, 1))
