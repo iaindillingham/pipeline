fe18e31da99e5615017b14bd703d99e25bce8716
Iain Dillingham <iain@dillingham.me.uk>
2025-01-13 20:40:01 +0000

    Add `object_hook`
    The hook should allow us to decode ISO 8601 format date strings into
    Python `datetime.date` objects in the `extract` stage. However, the
    hook's not exercised by our tests, because we patch `requests.get` with
    a mock response object.

--- pipeline.py
+++ pipeline.py
@@ -7,9 +7,18 @@ import requests
 Book = collections.namedtuple("Book", "publish_date")
 
 
+def object_hook(obj):
+    for key, value in obj.items():
+        try:
+            obj[key] = datetime.date.fromisoformat(value)
+        except Exception:
+            pass
+        return obj
+
+
 def extract():
     response = requests.get("https://example.com/")
-    page = response.json()
+    page = response.json(object_hook=object_hook)
     yield from page["records"]
 
 
